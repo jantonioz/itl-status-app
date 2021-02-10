793fc9e467696496fd9032a42dc0511888b6e7f5
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-09 20:43:55 -0600

    setup carga

--- src/components/HomeCarga.vue
+++ src/components/HomeCarga.vue
@@ -10,7 +10,31 @@
 						<v-col>
 							<div class="text-h2 mb-2">Carga</div>
 						</v-col>
+						<v-col cols="1">
+							<v-select
+								v-model="language"
+								:items="languages"
+								menu-props="auto"
+								label="Idioma"
+								hide-details
+								append-icon="mdi-web"
+								single-line
+								dense
+								dark
+								outlined
+								color="secondaryAlt"
+							>
+							</v-select>
+						</v-col>
 					</v-row>
+					<v-data-table
+						:headers="headersTable"
+						:items="cargaItems"
+						:items-per-page="5"
+						class="elevation-10"
+						dark
+					>
+					</v-data-table>
 				</v-col>
 			</v-row>
 		</v-container>
@@ -18,7 +42,66 @@
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+
+import { headers } from '../constants/carga.lang'
+
+export default {
+	data: () => ({
+		headersTable: [],
+		cargaItems: [],
+		language: 'ES',
+		languages: ['EN', 'ES'],
+	}),
+	computed: {
+		...mapGetters('user', {
+			user: 'getUser',
+			carga: 'getCarga',
+		}),
+	},
+	methods: {
+		mapSchedule(schedule) {
+			const result = Object.keys(schedule).reduce(
+				(acc, k) => ({
+					...acc,
+					[k]: `${schedule[k].from}-${schedule[k].to}/${schedule[k].room}`,
+				}),
+				{}
+			)
+			console.log('mapSchedule', this.headersTable, result)
+			return result
+		},
+		parseCarga(carga) {
+			return carga.map((c) => ({
+				...c,
+				key: c.subject.key,
+				...this.mapSchedule(c.schedule),
+			}))
+		},
+	},
+	watch: {
+		user(newUser) {
+			if (newUser.username && !this.carga.length) {
+				console.log(new Date().toISOString(), 'getStatus')
+				this.$store.dispatch('user/getStatus', this.user)
+			}
+		},
+		carga(newCarga) {
+			if (newCarga.length) {
+				this.headersTable = headers[this.language.toLowerCase()]
+
+				this.cargaItems = this.parseCarga(newCarga)
+			}
+		},
+		language(newLang) {
+			if (this.carga.length) {
+				this.headersTable = headers[newLang.toLowerCase()]
+
+				this.cargaItems = this.parseCarga(this.carga)
+			}
+		},
+	},
+}
 </script>
 
 <style></style>
---
+++ src/constants/carga.lang.js
@@ -0,0 +1,72 @@
+const headers = {
+	en: [
+		{
+			text: 'Subject Key',
+			value: 'key',
+		},
+		{
+			text: 'Group',
+			value: 'group',
+		},
+		{
+			text: 'Professor',
+			value: 'professor',
+		},
+		{
+			text: 'Monday',
+			value: '1',
+		},
+		{
+			text: 'Tuesday',
+			value: '2',
+		},
+		{
+			text: 'Wednesday',
+			value: '3',
+		},
+		{
+			text: 'Thursday',
+			value: '4',
+		},
+		{
+			text: 'Friday',
+			value: '5',
+		},
+	],
+	es: [
+		{
+			text: 'Clave',
+			value: 'key',
+		},
+		{
+			text: 'Group',
+			value: 'group',
+		},
+		{
+			text: 'Catedrático',
+			value: 'professor',
+		},
+		{
+			text: 'Lunes',
+			value: '1',
+		},
+		{
+			text: 'Martes',
+			value: '2',
+		},
+		{
+			text: 'Miércoles',
+			value: '3',
+		},
+		{
+			text: 'Jueves',
+			value: '4',
+		},
+		{
+			text: 'Viernes',
+			value: '5',
+		},
+	],
+}
+
+export { headers }
