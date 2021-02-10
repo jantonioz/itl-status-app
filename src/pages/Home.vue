2af129b31ff32ad3641dc5f98fbf85270ab1d04d
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-09 19:27:59 -0600

    setup home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,21 @@
 <template>
 	<div>
-		<h1>Home</h1>
+		<HomeCarga />
+		<HomeContent />
 		<AuthValidator />
 	</div>
 </template>
 
 <script>
 import AuthValidator from '../components/AuthValidator.vue'
+import HomeContent from '../components/HomeContent.vue'
+import HomeCarga from '../components/HomeCarga.vue'
 
 export default {
 	components: {
 		AuthValidator,
+		HomeContent,
+		HomeCarga
 	},
 }
 </script>
