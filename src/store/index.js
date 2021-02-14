b0992d41049326b4483ade13fffd547b0350344e
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-13 21:31:56 -0600

    added: security to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
+
+const ls = new SecureLS()
 
 import user from './modules/user'
 
@@ -13,5 +16,13 @@ export default new Vuex.Store({
 		user,
 	},
 	strict: debug,
-	plugins: [createPersistedState()],
+	plugins: [
+		createPersistedState({
+			storage: {
+				getItem: (key) => ls.get(key),
+				setItem: (key, value) => ls.set(key, value),
+				removeItem: (key) => ls.remove(key),
+			},
+		}),
+	],
 })
