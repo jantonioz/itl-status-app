cddb2b7ab95cd8fbcc49c4e877572e804ce142fb
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-13 21:08:28 -0600

    added: high charts

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import HighchartsVue from 'highcharts-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(HighchartsVue)
 
 new Vue({
   store,
