16e08512522ab4850d22eb929b55792f15a5d46e
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-13 21:22:45 -0600

    fix: improved user validation, alter on change

--- src/components/AuthValidator.vue
+++ src/components/AuthValidator.vue
@@ -3,11 +3,28 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-	mounted() {
-		if (!this.$store.user || !this.$store.user.username) {
-			this.$router.push('/login')
-		}
+	computed: {
+		...mapGetters('user', {
+			user: 'getUser',
+		}),
+	},
+	watch: {
+		user(user) {
+			this.checkUser(user)
+		},
+	},
+	methods: {
+		checkUser(user) {
+			console.log('checkUser', user)
+			if (!user || !user.username) {
+				this.$router.push('/login')
+			} else if (this.$route.path.indexOf('login')) {
+				this.$router.push('/')
+			}
+			console.log('checkUser', true, this.$route.path)
+		},
 	},
 }
 </script>
