7367b402757df05ed50158f63df33d5a989fe102
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-20 21:48:33 -0600

    fix: bug at path search in valid paths

--- src/components/AuthValidator.vue
+++ src/components/AuthValidator.vue
@@ -21,31 +21,34 @@ export default {
 		checkUser(user) {
 			// console.log('checkUser', user)
 			const isOnKnownRoute = this.validateRoute(this.$route.path)
-			
-			if (user && !user.username) {
+
+			if (user && !user.username && this.$route.path.indexOf('/login') === -1) {
 				this.$router.push('/login')
 			}
-			
-			if (!isOnKnownRoute) {
+
+			if (!isOnKnownRoute && this.$route.path.indexOf('/login') === -1) {
 				this.$router.push('/login')
 			}
 		},
 		validateRoute(path) {
 			if (!path || typeof path !== 'string' || path.length <= 0) return false
 			const normalPath = path.replace(/\//gi, '')
-			const route = routes.find(route => normalPath.indexOf(route))
+			const route = routes.find((route) => normalPath.indexOf(route) >= 0)
 			return route
-		}
+		},
 	},
 	mounted() {
-		if (this.user && !this.user.username) {
+		if (
+			this.user &&
+			!this.user.username &&
+			this.$route.path.indexOf('/login') === -1
+		) {
 			this.$router.push('/login')
-		} 
-		else this.checkUser(this.user)
+		} else this.checkUser(this.user)
 	},
 	created() {
 		this.checkUser(this.user)
-	}
+	},
 }
 </script>
 
